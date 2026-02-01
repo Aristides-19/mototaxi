0ab9fcad664a0dad9f7bf88ad6d929f0b9465300
Arístides Pérez <aristp.2005@gmail.com>
2026-02-01 16:21:28 -0400

    feat: add BikeControllerSc to use InputSystem correctly

---
+++ Assets/Scripts/BikeControllerSc.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using rayzngames;
+
+[RequireComponent(typeof(BicycleVehicle))]
+public class BikeControllerSc : MonoBehaviour
+{
+
+    [SerializeField] bool controllingBike;
+
+    BicycleVehicle bicycle;
+
+    InputAction moveInput;
+    InputAction brakeInput;
+
+    void Awake()
+    {
+        bicycle = GetComponent<BicycleVehicle>();
+        moveInput = InputSystem.actions.FindAction("Move");
+        brakeInput = InputSystem.actions.FindAction("Brake");
+    }
+
+    void Update()
+    {
+        Vector2 inputVector = moveInput.ReadValue<Vector2>();
+
+        bicycle.verticalInput = inputVector.y;
+        bicycle.horizontalInput = inputVector.x;
+
+        bicycle.braking = brakeInput.IsPressed();
+
+        bicycle.InControl(controllingBike);
+
+        if (controllingBike) bicycle.ConstrainRotation(bicycle.OnGround());
+        else bicycle.ConstrainRotation(false);
+
+    }
+}
