394850e7f38343c67607fb1bdb3936cf823648b3
Arístides Pérez <aristp.2005@gmail.com>
2026-02-08 23:46:02 -0400

    feat: add low poly cars package

---
+++ Assets/Scripts/Editor/AddComponentToChildrenSc.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+
+public class AddMeshColliderToChildren : EditorWindow
+{
+    [MenuItem("Tools/Add Mesh Collider Convex To Children")]
+    public static void AddComponent()
+    {
+        GameObject[] selectedParents = Selection.gameObjects;
+
+        if (selectedParents.Length == 0)
+        {
+            Debug.LogWarning("Please select a parent object first.");
+            return;
+        }
+
+        foreach (GameObject parent in selectedParents)
+        {
+            MeshFilter[] childrenWithMesh = parent.GetComponentsInChildren<MeshFilter>();
+
+            foreach (MeshFilter meshFilter in childrenWithMesh)
+            {
+                GameObject child = meshFilter.gameObject;
+
+                if (child.TryGetComponent(out MeshCollider meshCol) == false)
+                {
+                    meshCol = child.AddComponent<MeshCollider>();
+                }
+
+                meshCol.convex = true;
+            }
+
+            Debug.Log($"Component added to the children of {parent.name}");
+        }
+
+
+    }
+}
