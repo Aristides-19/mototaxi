36ce0919b650d87a859040de0734204a558c8022
Arístides Pérez <aristp.2005@gmail.com>
2026-02-27 13:12:21 -0400

    feat: add RoceMechanicSc to manage roce score in-game with raycast + trigger

---
+++ Assets/Scripts/Mechanics/RoceMechanicSc.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Mototaxi.Core;
+using UnityEngine;
+
+namespace Mototaxi.Mechanics
+{
+    [RequireComponent(typeof(Collider))]
+    public class RoceMechanicSc : MonoBehaviour
+    {
+        [SerializeField] Rigidbody playerRigidbody;
+        [SerializeField] GameDataSc GameData;
+        private readonly HashSet<Collider> collidedTraffic = new();
+
+        void Awake()
+        {
+            Collider collider = GetComponent<Collider>();
+            collider.isTrigger = true;
+
+            if (playerRigidbody == null)
+            {
+                Debug.LogError("Player Rigidbody reference is missing in RoceMechanicSc.");
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            Vector3 direction = other.bounds.center - transform.position;
+
+            if (Physics.Raycast(transform.position, direction, out RaycastHit hit, GameData.RoceSettings.DistanceThreshold, GameData.TrafficLayer))
+            {
+                // Just register a roce if the raycast hits the traffic collider and the player is moving fast enough
+                if (hit.collider == other && !collidedTraffic.Contains(other) && playerRigidbody.linearVelocity.magnitude > 4f)
+                {
+                    Debug.DrawRay(transform.position, direction, Color.red);
+                    collidedTraffic.Add(other);
+                    ScoreManagerSc.AddScore(GameData.RoceSettings.ScoreMultiplier / Mathf.Max(hit.distance, 0.1f));
+                }
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            collidedTraffic.Remove(other);
+        }
+    }
+}
