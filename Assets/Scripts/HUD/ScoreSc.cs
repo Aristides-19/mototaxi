6230ee99638b56094b2198858758cf789117e609
Arístides Pérez <aristp.2005@gmail.com>
2026-03-08 17:06:57 -0400

    feat: add ScoreSource feedback to HUD with ScoreSc

--- Assets/Scripts/HUD/ScoreSc.cs
+++ Assets/Scripts/HUD/ScoreSc.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using Mototaxi.Core;
 using TMPro;
 using UnityEngine;
@@ -10,9 +9,10 @@ namespace Mototaxi.HUD
     {
         [Header("Score Display")]
         [SerializeField] TextMeshProUGUI scoreText;
+        [SerializeField] TextMeshProUGUI scoreChangeText;
+        [SerializeField] TextMeshProUGUI scoreSourceText;
 
         [Header("Opacity Settings")]
-        [SerializeField] TextMeshProUGUI scoreChangeText;
         [SerializeField] float fadeDuration = 0.5f;
         [SerializeField] float displayDuration = 1f;
         [SerializeField] float maxAlpha = 1f;
@@ -21,10 +21,14 @@ namespace Mototaxi.HUD
         [SerializeField] float punchScale = 1.3f;
         [SerializeField] float punchDuration = 0.1f;
 
-        private Vector3 originalScale;
+        // ANCHOR: Score Change Related
+        private Vector3 originalScoreChangeScale;
         private float currentScoreChange;
-        private Coroutine fadeCoroutine;
-        private Coroutine punchCoroutine;
+        private Coroutine fadeScoreChangeCoroutine;
+        private Coroutine punchScoreChangeCoroutine;
+
+        // ANCHOR: Score Source Related
+        private Coroutine fadeScoreSourceCoroutine;
 
         private void Awake()
         {
@@ -33,9 +37,10 @@ namespace Mototaxi.HUD
                 Debug.LogError("Score TextMeshProUGUI reference is missing in ScoreSc.");
             }
 
-            originalScale = scoreChangeText.transform.localScale;
+            originalScoreChangeScale = scoreChangeText.transform.localScale;
 
             TMPUtilsSc.SetTextAlpha(scoreChangeText, 0f);
+            TMPUtilsSc.SetTextAlpha(scoreSourceText, 0f);
             HandleScore(ScoreManagerSc.CurrentScore, 0f, ScoreSource.None);
 
         }
@@ -52,12 +57,33 @@ namespace Mototaxi.HUD
         private void HandleScore(float score, float change, ScoreSource source)
         {
             UpdateScore(score);
+            // NOTE: It doesn't handle negative score changes
             AddScoreChange(change);
+            AddScoreSource(change, source);
         }
 
         private void UpdateScore(float score)
         {
             scoreText.text = $"Bs. <size=+20>{MathF.Round(score, 2)}</size>";
+        }
+
+        private void AddScoreSource(float change, ScoreSource source)
+        {
+            if (change <= 0) return;
+
+            float roundedChange = MathF.Round(change, 2);
+
+            scoreSourceText.text = source switch
+            {
+                ScoreSource.Roce => $"+{roundedChange} ROCE",
+                ScoreSource.Wheelie => $"+{roundedChange} CABALLITO",
+                _ => ""
+            };
+
+            TMPUtilsSc.SetTextAlpha(scoreSourceText, maxAlpha);
+
+            if (fadeScoreSourceCoroutine != null) StopCoroutine(fadeScoreSourceCoroutine);
+            fadeScoreSourceCoroutine = StartCoroutine(TMPUtilsSc.FadeRoutine(displayDuration, fadeDuration, maxAlpha, scoreSourceText, () => fadeScoreSourceCoroutine = null));
         }
 
         private void AddScoreChange(float change)
@@ -69,15 +95,15 @@ namespace Mototaxi.HUD
 
             TMPUtilsSc.SetTextAlpha(scoreChangeText, maxAlpha);
 
-            if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
-            fadeCoroutine = StartCoroutine(TMPUtilsSc.FadeRoutine(displayDuration, fadeDuration, maxAlpha, scoreChangeText, () =>
+            if (fadeScoreChangeCoroutine != null) StopCoroutine(fadeScoreChangeCoroutine);
+            fadeScoreChangeCoroutine = StartCoroutine(TMPUtilsSc.FadeRoutine(displayDuration, fadeDuration, maxAlpha, scoreChangeText, () =>
             {
                 currentScoreChange = 0;
-                fadeCoroutine = null;
+                fadeScoreChangeCoroutine = null;
             }));
 
-            if (punchCoroutine != null) StopCoroutine(punchCoroutine);
-            punchCoroutine = StartCoroutine(TMPUtilsSc.PunchScaleRoutine(originalScale, punchScale, punchDuration, scoreChangeText, () => punchCoroutine = null));
+            if (punchScoreChangeCoroutine != null) StopCoroutine(punchScoreChangeCoroutine);
+            punchScoreChangeCoroutine = StartCoroutine(TMPUtilsSc.PunchScaleRoutine(originalScoreChangeScale, punchScale, punchDuration, scoreChangeText, () => punchScoreChangeCoroutine = null));
         }
     }
 }
