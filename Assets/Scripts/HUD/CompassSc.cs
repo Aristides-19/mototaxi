68c5bb76f62418bc8e08b9bba9a25a5c7184939c
Arístides Pérez <aristp.2005@gmail.com>
2026-03-07 23:24:34 -0400

    feat: add box border to Compass, and added to Road scene with rotator ref

---
+++ Assets/Scripts/HUD/CompassSc.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mototaxi.HUD
+{
+    public class CompassSc : MonoBehaviour
+    {
+        [Header("References")]
+        [Tooltip("Transform of the player or object whose rotation will be used to update the compass")]
+        public Transform playerRotator;
+
+        [Tooltip("Compass RawImage component to update the UV rect")]
+        public RawImage compassTextureUV;
+
+        void Awake()
+        {
+            if (playerRotator == null)
+            {
+                Debug.LogError("playerRotator is not assigned in the inspector.");
+            }
+            if (compassTextureUV == null)
+            {
+                Debug.LogError("compassTextureUV is not assigned in the inspector.");
+            }
+        }
+
+        void Update()
+        {
+            float gradosY = playerRotator.eulerAngles.y;
+            float posicionUV = (gradosY + 180f) / 360f;
+            compassTextureUV.uvRect = new Rect(posicionUV, 0f, 1f, 1f);
+        }
+    }
+}
