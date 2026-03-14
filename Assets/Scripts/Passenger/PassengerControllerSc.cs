a168af2b0bf0167eeabda9e8e3b75796701cb557
Arístides Pérez <aristp.2005@gmail.com>
2026-03-14 19:09:47 -0400

    feat: add PassengerControllerSc and PassengerPoolSO to manage passenger spawning in-road

---
+++ Assets/Scripts/Passenger/PassengerControllerSc.cs
@@ -0,0 +1,50 @@
+using Mototaxi.Trips;
+using UnityEngine;
+
+namespace Mototaxi.Passenger
+{
+    public class PassengerController : MonoBehaviour
+    {
+        [SerializeField] private SkinnedMeshRenderer _meshRenderer;
+        private PassengerPoolSO _pool;
+        private PointSc _spawnPoint;
+        private PointSc _destinationPoint;
+        private PassengerDataSO _currentData;
+
+        public PassengerDataSO CurrentData => _currentData;
+        public PointSc DestinationPoint => _destinationPoint;
+
+        public void Init(PassengerPoolSO pool, PassengerDataSO data, PointSc spawnPoint, PointSc destinationPoint)
+        {
+            _pool = pool;
+            _currentData = data;
+            _spawnPoint = spawnPoint;
+            _destinationPoint = destinationPoint;
+
+            ApplyData(data);
+
+            transform.SetPositionAndRotation(spawnPoint.Position, spawnPoint.Rotation);
+            _spawnPoint.isOccupied = true;
+        }
+
+        private void ApplyData(PassengerDataSO data)
+        {
+            if (data.Mesh != null)
+            {
+                _meshRenderer.sharedMesh = data.Mesh;
+            }
+            else
+            {
+                Debug.LogWarning("PassengerDataSO Mesh is null. Please assign a valid mesh to the PassengerDataSO.");
+            }
+        }
+
+        public void Despawn()
+        {
+            _spawnPoint.isOccupied = false;
+            _spawnPoint = null;
+            _destinationPoint = null;
+            _pool.ReturnPassenger(this);
+        }
+    }
+}
