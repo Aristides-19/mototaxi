743d832240968617a1328abc0c9eebc5e5571ee5
Arístides Pérez <aristp.2005@gmail.com>
2026-03-26 03:56:18 -0400

    feat: add AlarmSc and sounds for game over ragdoll crashes

--- Assets/Scripts/Sounds/AlarmSc.cs
+++ Assets/Scripts/Sounds/AlarmSc.cs
@@ -5,15 +5,15 @@ using System.Collections.Generic;
 namespace Mototaxi.Sounds
 {
     [RequireComponent(typeof(AudioSource))]
-    public class CrashSc : MonoBehaviour
+    public class AlarmSc : MonoBehaviour
     {
         [Header("References")]
         [Tooltip("Reference to the RagdollActivator component.")]
         [SerializeField] private RagdollActivator ragdollActivator;
 
         [Header("Audio Settings")]
-        [Tooltip("List of crash sound effects to play randomly.")]
-        [SerializeField] private List<AudioClip> crashSounds;
+        [Tooltip("List of alarm sound effects to play randomly.")]
+        [SerializeField] private List<AudioClip> alarmSounds;
 
         [Tooltip("Minimum pitch to apply for variation.")]
         [SerializeField, Range(0.5f, 1.5f)] private float minPitch = 0.85f;
@@ -30,17 +30,17 @@ namespace Mototaxi.Sounds
 
         private void OnEnable()
         {
-            ragdollActivator.OnRagdollActivated += PlayCrashSound;
+            ragdollActivator.OnRagdollActivated += PlayAlarmSound;
         }
 
         private void OnDisable()
         {
-            ragdollActivator.OnRagdollActivated -= PlayCrashSound;
+            ragdollActivator.OnRagdollActivated -= PlayAlarmSound;
         }
 
-        private void PlayCrashSound()
+        private void PlayAlarmSound()
         {
-            AudioClip clip = crashSounds[Random.Range(0, crashSounds.Count)];
+            AudioClip clip = alarmSounds[Random.Range(0, alarmSounds.Count)];
 
             audioSource.pitch = Random.Range(minPitch, maxPitch);
 
